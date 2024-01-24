251c2df384f4c6bf2cde9e15803e1be169d3d645
Luis Martinez <luis.martinez@newfold.com>
2024-01-24 10:04:47 -0400

    update: new approach to open db

--- hooks/useDatabase.ts
+++ hooks/useDatabase.ts
@@ -5,6 +5,7 @@ import { Asset } from "expo-asset";
 import { DBName } from "../enums";
 import { ToastAndroid } from "react-native";
 import { CHECK_DB } from "constants/Queries";
+import { EBibleVersions } from "types";
 
 interface Row {
   [key: string]: any;
@@ -95,87 +96,177 @@ function useDatabase({ dbNames }: TUseDatabase): UseDatabase {
   };
 
   useEffect(() => {
-    if (!Array.isArray(dbNames)) return;
-    const dbExists = async (dbName: string) => {
-      return new Promise((resolve, reject) => {
-        const db = SQLite.openDatabase(dbName);
-        db.transaction(
-          (tx) => {
-            tx.executeSql(CHECK_DB, [], (_, result) => {
-              resolve(result.rows.length > 0);
-            });
-          },
-          () => {
-            resolve(false);
-            return false;
-          },
-          () => {
-            db.closeAsync();
-          }
-        );
-      });
-    };
+    async function openDatabase(databaseName: string) {
+      const localFolder = FileSystem.documentDirectory + "SQLite";
+      const dbName = databaseName;
+      const localURI = localFolder + "/" + dbName;
 
-    const isDB = async () => {
-      const dbFolder = `${FileSystem.documentDirectory}SQLite`;
-      if (!(await FileSystem.getInfoAsync(dbFolder)).exists) {
-        await FileSystem.makeDirectoryAsync(dbFolder);
+      if (!(await FileSystem.getInfoAsync(localFolder)).exists) {
+        await FileSystem.makeDirectoryAsync(localFolder);
       }
 
-      for (const dbName of dbNames) {
-        if (!dbName) return;
-        const exists = await dbExists(dbName);
-        if (!exists) {
-          return false;
+      let asset =
+        dbName === DBName.BIBLE
+          ? Asset.fromModule(require("../assets/db/bible.db"))
+          : Asset.fromModule(require("../assets/db/ntv-bible.db"));
+
+      const name =
+        dbName === DBName.BIBLE ? EBibleVersions.RVR60 : EBibleVersions.NTV;
+      if (!asset.downloaded) {
+        await asset.downloadAsync().then((value) => {
+          asset = value;
+          console.log("asset downloadAsync - finished");
+          ToastAndroid.show(name, ToastAndroid.SHORT);
+        });
+
+        let remoteURI = asset.localUri;
+
+        if (!(await FileSystem.getInfoAsync(localURI)).exists) {
+          await FileSystem.copyAsync({
+            from: remoteURI as string,
+            to: localURI,
+          }).catch((error) => {
+            console.log("asset copyDatabase - finished with error: " + error);
+          });
         }
-      }
+      } else {
+        // for iOS - Asset is downloaded on call Asset.fromModule(), just copy from cache to local file
+        if (
+          asset.localUri ||
+          asset.uri.startsWith("asset") ||
+          asset.uri.startsWith("file")
+        ) {
+          let remoteURI = asset.localUri || asset.uri;
 
-      return true;
-    };
+          if (!(await FileSystem.getInfoAsync(localURI)).exists) {
+            await FileSystem.copyAsync({
+              from: remoteURI,
+              to: localURI,
+            }).catch((error) => {
+              console.log("local copyDatabase - finished with error: " + error);
+            });
+          }
+        } else if (
+          asset.uri.startsWith("http") ||
+          asset.uri.startsWith("https")
+        ) {
+          let remoteURI = asset.uri;
 
-    const openDB = async () => {
-      const _isDB = await isDB();
-      if (!_isDB) {
-        await copyDatabases(dbNames);
-        setDbCreated(true);
-        return "";
-      }
-
-      const databases: SQLite.SQLiteDatabase[] = [];
-      for (const dbName of dbNames) {
-        const db = await new Promise<SQLite.SQLiteDatabase>(
-          (resolve, reject) => {
-            const database = SQLite.openDatabase(
-              dbName,
-              undefined,
-              undefined,
-              undefined,
-              (result) => {
-                if (result) {
-                  console.log(`Database ${dbName} opened successfully.`);
-                  resolve(database);
-                } else {
-                  console.error(`Error opening database ${dbName}.`);
-                  reject(new Error(`Error opening database ${dbName}.`));
-                }
+          if (!(await FileSystem.getInfoAsync(localURI)).exists) {
+            await FileSystem.downloadAsync(remoteURI, localURI).catch(
+              (error) => {
+                console.log(
+                  "local downloadAsync - finished with error: " + error
+                );
               }
             );
           }
-        );
+        }
+      }
 
+      return SQLite.openDatabase(dbName);
+    }
+
+    async function openDatabases() {
+      const databases: SQLite.SQLiteDatabase[] = [];
+      for (const dbName of dbNames) {
+        const db = await openDatabase(dbName);
         databases.push(db);
       }
       return databases;
-    };
+    }
 
-    openDB()
+    openDatabases()
       .then((resultDatabases: SQLite.SQLiteDatabase[] | string) => {
         if (Array.isArray(resultDatabases)) {
           setDatabases(resultDatabases);
         }
       })
       .catch(console.log);
-  }, [isDbCreated]);
+  }, []);
+
+  // useEffect(() => {
+  //   if (!Array.isArray(dbNames)) return;
+  //   const dbExists = async (dbName: string) => {
+  //     return new Promise((resolve, reject) => {
+  //       const db = SQLite.openDatabase(dbName);
+  //       db.transaction(
+  //         (tx) => {
+  //           tx.executeSql(CHECK_DB, [], (_, result) => {
+  //             resolve(result.rows.length > 0);
+  //           });
+  //         },
+  //         () => {
+  //           resolve(false);
+  //           return false;
+  //         },
+  //         () => {
+  //           db.closeAsync();
+  //         }
+  //       );
+  //     });
+  //   };
+
+  //   const isDB = async () => {
+  //     const dbFolder = `${FileSystem.documentDirectory}SQLite`;
+  //     if (!(await FileSystem.getInfoAsync(dbFolder)).exists) {
+  //       await FileSystem.makeDirectoryAsync(dbFolder);
+  //     }
+
+  //     for (const dbName of dbNames) {
+  //       if (!dbName) return;
+  //       const exists = await dbExists(dbName);
+  //       if (!exists) {
+  //         return false;
+  //       }
+  //     }
+
+  //     return true;
+  //   };
+
+  //   const openDB = async () => {
+  //     const _isDB = await isDB();
+  //     if (!_isDB) {
+  //       await copyDatabases(dbNames);
+  //       setDbCreated(true);
+  //       return "";
+  //     }
+
+  //     const databases: SQLite.SQLiteDatabase[] = [];
+  //     for (const dbName of dbNames) {
+  //       const db = await new Promise<SQLite.SQLiteDatabase>(
+  //         (resolve, reject) => {
+  //           const database = SQLite.openDatabase(
+  //             dbName,
+  //             undefined,
+  //             undefined,
+  //             undefined,
+  //             (result) => {
+  //               if (result) {
+  //                 console.log(`Database ${dbName} opened successfully.`);
+  //                 resolve(database);
+  //               } else {
+  //                 console.error(`Error opening database ${dbName}.`);
+  //                 reject(new Error(`Error opening database ${dbName}.`));
+  //               }
+  //             }
+  //           );
+  //         }
+  //       );
+
+  //       databases.push(db);
+  //     }
+  //     return databases;
+  //   };
+
+  //   openDB()
+  //     .then((resultDatabases: SQLite.SQLiteDatabase[] | string) => {
+  //       if (Array.isArray(resultDatabases)) {
+  //         setDatabases(resultDatabases);
+  //       }
+  //     })
+  //     .catch(console.log);
+  // }, [isDbCreated]);
 
   return { executeSql, databases: _databases };
 }
