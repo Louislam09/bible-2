594fc2527fc43ba8ac6021501aa179b36c181d5f
Luis Martinez <luis.martinez@newfold.com>
2024-01-24 10:11:57 -0400

    log 4

--- screens/Log.tsx
+++ screens/Log.tsx
@@ -2,19 +2,14 @@ import { Text, View } from "components/Themed";
 import { useDBContext } from "context/databaseContext";
 import React, { useEffect, useState } from "react";
 import * as FileSystem from "expo-file-system";
-import { DBName } from "enums";
-import { Asset } from "expo-asset";
 
 const LogScreen = () => {
   const { myBibleDB, executeSql } = useDBContext();
   const [tables, setTables] = useState<any>([]);
+  const dbName = (myBibleDB as any)?._db?._name;
 
-  const fileName = `SQLite/bible.db`;
+  const fileName = `SQLite/${dbName}`;
   const filePath = `${FileSystem.documentDirectory}${fileName}`;
-  const filePath2 = `${Asset.fromModule(require(`../assets/db/bible.db`)).uri}`;
-  const filePath3 = `${
-    Asset.fromModule(require(`../assets/db/ntv-bible.db`)).uri
-  }`;
 
   useEffect(() => {
     if (!myBibleDB || !executeSql) return;
@@ -27,10 +22,8 @@ const LogScreen = () => {
 
   return (
     <View>
-      <Text>{`Name: ${(myBibleDB as any)?._db?._name}`}</Text>
+      <Text>{`Name: ${dbName}`}</Text>
       <Text>{`Path: ${filePath}`}</Text>
-      <Text>{`${DBName.BIBLE}: ${filePath2}`}</Text>
-      <Text>{`${DBName.NTV}: ${filePath3}`}</Text>
       <Text>Tables:</Text>
       {tables.map((x: any) => (
         <Text key={x?.name}>{x?.name}</Text>
