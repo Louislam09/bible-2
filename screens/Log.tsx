9a6accbf80f32e9db89480149234a48a88ff0271
Luis Martinez <luis.martinez@newfold.com>
2024-01-24 02:56:17 -0400

    log

--- screens/Log.tsx
+++ screens/Log.tsx
@@ -2,12 +2,15 @@ import { Text, View } from "components/Themed";
 import { useDBContext } from "context/databaseContext";
 import React, { useEffect, useState } from "react";
 import * as FileSystem from "expo-file-system";
+import { DBName } from "enums";
 
 const LogScreen = () => {
   const { myBibleDB, executeSql } = useDBContext();
   const [tables, setTables] = useState<any>([]);
   const fileName = `SQLite/${(myBibleDB as any)?._db?._name}`;
   const filePath = `${FileSystem.documentDirectory}${fileName}`;
+  const filePath2 = `${FileSystem.documentDirectory}${DBName.BIBLE}`;
+  const filePath3 = `${FileSystem.documentDirectory}${DBName.NTV}`;
 
   useEffect(() => {
     if (!myBibleDB || !executeSql) return;
@@ -22,6 +25,8 @@ const LogScreen = () => {
     <View>
       <Text>{`Name: ${(myBibleDB as any)?._db?._name}`}</Text>
       <Text>{`Path: ${filePath}`}</Text>
+      <Text>{`${DBName.BIBLE}: ${filePath2}`}</Text>
+      <Text>{`${DBName.NTV}: ${filePath3}`}</Text>
       <Text>Tables:</Text>
       {tables.map((x: any) => (
         <Text key={x?.name}>{x?.name}</Text>
