edcb9011c4f2490ddc856d8a30ac767ac490d0d6
Luis Martinez <louislam09@hotmail.com>
2024-10-25 14:18:56 -0400

    update packages

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -1,29 +1,10 @@
-/**
- * Learn more about Light and Dark modes:
- * https://docs.expo.io/guides/color-schemes/
- */
-
 import { Text as DefaultText, View as DefaultView } from "react-native";
 
-// import Colors from "../constants/Colors";
-// import useColorScheme from "../hooks/useColorScheme";
 import { useBibleContext } from "../context/BibleContext";
 import { useTheme } from "@react-navigation/native";
 import { TTheme } from "types";
 
-// export function useThemeColor(
-//   props: { light?: string; dark?: string },
-//   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-// ) {
-//   const theme = useColorScheme();
-//   const colorFromProps = props[theme];
-
-//   if (colorFromProps) {
-//     return colorFromProps;
-//   } else {
-//     return Colors[theme][colorName];
-//   }
-// }
+export const NativeDefaultView = require('react-native/Libraries/Components/View/ViewNativeComponent').default;
 
 type ThemeProps = {
   lightColor?: string;
@@ -35,7 +16,6 @@ export type ViewProps = ThemeProps & DefaultView["props"];
 
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  // const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
   const { colors } = useTheme() as TTheme;
   const { selectedFont } = useBibleContext();
   return (
@@ -56,13 +36,9 @@ export function Text(props: TextProps) {
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const { colors } = useTheme() as TTheme;
-  // const backgroundColor = useThemeColor(
-  //   { light: lightColor, dark: darkColor },
-  //   "background"
-  // );
 
   return (
-    <DefaultView
+    <NativeDefaultView
       style={[{ backgroundColor: colors.background }, style]}
       {...otherProps}
     />
