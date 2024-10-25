21bcde7dd57066993b74478415189b82d274257b
Luis Martinez <louislam09@hotmail.com>
2024-10-25 13:00:00 -0400

    update: skeleton color

--- components/home/content/SkeletonVerse.tsx
+++ components/home/content/SkeletonVerse.tsx
@@ -9,32 +9,29 @@ const SkeletonVerse = ({ index }: any) => {
 
     return (
         <MotiView
-            transition={{
-                type: 'timing',
-                duration: 300,
-            }}
+            transition={{ type: 'timing', duration: 300 }}
             animate={{ backgroundColor: theme.colors.background }}
             style={{ paddingHorizontal: 10 }}
         >
             <View style={[{ display: index === 0 ? 'flex' : 'none', alignItems: 'flex-end', justifyContent: 'flex-end', paddingRight: 10 }]} >
-                <Skeleton colorMode={colorMode} height={15} width={150} />
+                <Skeleton colorMode={colorMode} backgroundColor={theme.colors.text + 30} height={15} width={150} />
                 <Spacer />
             </View>
 
             <View style={{ display: index === 0 ? 'flex' : 'none', alignItems: 'center', justifyContent: 'center' }}>
-                <Skeleton colorMode={colorMode} height={15} width={250} />
+                <Skeleton colorMode={colorMode} backgroundColor={theme.colors.text + 30} height={15} width={250} />
                 <Spacer />
             </View>
 
             <View style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', gap: 10 }}>
                 <Spacer height={8} />
-                <Skeleton colorMode={colorMode} radius="round" height={20} width={20} />
-                <Skeleton colorMode={colorMode} width={'93%'} height={20} />
+                <Skeleton colorMode={colorMode} backgroundColor={theme.colors.text + 30} radius="round" height={20} width={20} />
+                <Skeleton colorMode={colorMode} backgroundColor={theme.colors.text + 30} width={'93%'} height={20} />
             </View>
             <View style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
                 <Spacer height={4} />
-                <Skeleton colorMode={colorMode} width={'95%'} height={20} />
-                <Skeleton colorMode={colorMode} width={'95%'} height={20} />
+                <Skeleton colorMode={colorMode} backgroundColor={theme.colors.text + 30} width={'95%'} height={20} />
+                <Skeleton colorMode={colorMode} backgroundColor={theme.colors.text + 30} width={'95%'} height={20} />
                 <Spacer height={20} />
             </View>
         </MotiView>
