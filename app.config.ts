648e20f61dcf4bb11107df3eaa172ed5692d6839
Luis Martinez <luis.martinez@newfold.com>
2025-05-09 20:43:34 -0400

    feat: update runtimeVersion policy to appVersion and add appVersionSource in eas.json

--- app.config.ts
+++ app.config.ts
@@ -100,7 +100,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       'expo-router',
     ],
     runtimeVersion: {
-      policy: 'nativeVersion',
+      policy: 'appVersion',
     },
   };
 };
