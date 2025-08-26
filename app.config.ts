cbdab19234dfa50d02ec00ce39221affbba7078e
Luis Martinez <luis.martinez@newfold.com>
2025-08-25 22:19:41 -0400

    Toggle development mode configuration to use environment variable for better flexibility in app variants.

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ConfigContext, ExpoConfig } from "@expo/config";
 
-const IS_DEV = true;
-// const IS_DEV = process.env.APP_VARIANT === "development";
+// const IS_DEV = true;
+const IS_DEV = process.env.APP_VARIANT === "development";
 const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 // const IS_PREVIEW = true
 
