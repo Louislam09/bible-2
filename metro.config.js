69cc698479c04128c56b130922c0cac648bcd6c8
Luis Martinez <luis.martinez@newfold.com>
2025-09-02 11:45:25 -0400

    feat: Add wasm asset support and enhance middleware for SharedArrayBuffer headers

--- metro.config.js
+++ metro.config.js
@@ -9,6 +9,18 @@ defaultConfig.resolver.assetExts.push("mp3");
 defaultConfig.resolver.assetExts.push("SQLite3");
 defaultConfig.resolver.assetExts.push("css");
 
+// Add wasm asset support
+defaultConfig.resolver.assetExts.push('wasm');
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+defaultConfig.server.enhanceMiddleware = (middleware) => {
+    return (req, res, next) => {
+        res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+        middleware(req, res, next);
+    };
+};
+
 defaultConfig.transformer.assetPlugins = ["expo-asset/tools/hashAssetFiles"];
 // defaultConfig.unstable_enablePackageExports = false;
 
